658239d3d7b7cbe2384b55040f9cf9b6167d5639
Zev Isert <zev.isert@gmail.com>
2016-02-13 00:59:04 -0800

    Fix Double Digest

--- js/app.routes.js
+++ js/app.routes.js
@@ -11,25 +11,18 @@ angular.module('appRouter', ['ngRoute'])
 
 	.when('/home', {
 		templateUrl: 'partials/home.html',
-		controller: 'homeController',
-		controllerAs: 'home'
 	})
 
 	.when('/about', {
 		templateUrl: 'partials/about.html',
-		controller : 'aboutController',
-		controllerAs: 'about'
 	})
 
 	.when('/blog', {
 		templateUrl: 'partials/blog.html',
-		controller: 'blogController',
-		controllerAs: 'blog'
 	})
 
 	.when('/blog/:date', {
 		templateUrl: 'partials/view_post.html',
-		controller: 'postController'
 	})
 
 	.otherwise({
